bc26d733300db5bbfc94dedaf87d41dc5f599a66
Ibrahim Gerbiqi <34714009+IbrahimG99@users.noreply.github.com>
2020-09-19 01:59:45 +0200

    first demo with txt and images sorting to different folders

--- Auto-File-sort/Auto_File_sort.py
+++ Auto-File-sort/Auto_File_sort.py
@@ -9,16 +9,22 @@ class MyHandler(FileSystemEventHandler):
         def on_modified(self,event):
             for filename in os.listdir(folder_to_track):
                 name, extension = os.path.splitext(filename)
-                print(extension)
-                print(name)
-                src = folder_to_track + "/" + filename
-                new_destination = folder_destination + "/" + filename
-                os.rename(src, new_destination)
+                print(filename,name,extension)
+                if extension == ".txt":
+                    src = folder_to_track + "/" + filename
+                    new_destination = folder_txt_destination + "/" + filename
+                    os.rename(src, new_destination)
+                if (extension == ".jpg" or extension == ".png"): 
+                    src = folder_to_track + "/" + filename
+                    new_destination = folder_images_destination + "/" + filename
+                    os.rename(src, new_destination)
 
 
 
-folder_to_track = "/Users/Lenovo/Desktop/myFolder"
-folder_destination = "/Users/Lenovo/Desktop/newFolder"
+folder_to_track = "/Users/Lenovo/Desktop/"
+folder_txt_destination = "/Users/Lenovo/Desktop/txtFolder"
+folder_images_destination = "/Users/Lenovo/Desktop/imagesFolder"
+
 evenet_handler = MyHandler()
 observer = Observer()
 observer.schedule(evenet_handler, folder_to_track, recursive=True)
@@ -27,6 +33,7 @@ observer.start()
 try:
     while True:
         time.sleep(2)
+        print('its true')
 except KeyboardInterrupt:
     observer.stop()
 observer.join()
